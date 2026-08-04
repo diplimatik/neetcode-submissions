3a0bb2a828cc6ab823cc6b32ef319f96359d785d
diplimatik <amitheprogram@gmail.com>
2026-08-04 22:25:07 +0300

    Add: permutation-string - submission-11

---
+++ Data Structures & Algorithms/permutation-string/submission-11.go
@@ -0,0 +1,22 @@
+func checkInclusion(s1 string, s2 string) bool {
+	if len(s1) > len(s2) {
+		return false
+	}
+	s1Arr := [26]int{}
+	s2Arr := [26]int{}
+	for i := 0; i < len(s1); i++ {
+		s1Arr[s1[i]-'a']++
+		s2Arr[s2[i]-'a']++
+	}
+
+	for l, r := 0, len(s1)-1; r < len(s2)-1; l++ {
+		if s1Arr != s2Arr {
+			r++
+			s2Arr[s2[l]-'a']--
+			s2Arr[s2[r]-'a']++
+		} else {
+			return true
+		}
+	}
+	return s1Arr == s2Arr
+}
